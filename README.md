5e3673ebbdef5cdb1b88b04c7184fc6b04ddb8a5
Charles Lee <chug2k@gmail.com>
2019-03-03 15:40:56 +0700

    Initial commit, with README.md and boilerplate code setup.

---
+++ README.md
@@ -0,0 +1,64 @@
+# CoderSchool FTW - * Nam, the Monster Hunter *
+
+Created with love by: Charles Lee
+  
+This is a simple Canvas-based game that requires a solid understanding of the principles of JavaScript programming to customize. 
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories.
+
+```
+<img src='http://http://g.recordit.co/cURdPMSHT2.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+```
+
+
+## Required User Stories
+
+- [x] The user can move their character with the arrow keys. 
+- [x] The user can see a monster.
+- [ ] The monster is placed at a random location within the game boundaries.
+- [x] The user can catch a monster by moving their character to the same location as a monster.
+- [ ] When the user catches a monster, a new monster is placed randomly upon the screen.
+- [ ] The user can see the numbers of monsters she has caught. Every time she catches a monster, the number should increment by one. 
+- [ ] The game ends when the user has caught 20 monsters. The total time elapsed is displayed upon game completion, in seconds.
+- [ ] The user can access this game from the internet (e.g. using Netlify).
+- [ ] The code has been reviewed by at least one other person, using Pull Requests on GitHub.
+
+## Optional User Stories
+
+- [ ] Custom styling: the developer has replaced the images for the main character, monster, and background with something of her choice.  
+- [ ] The user cannot move her character off the screen. 
+- [ ] The user sees a few different types of monster each time a new monster is spawned. 
+- [ ] The monster moves around on its own each frame, and the user must chase it. 
+- [ ] The map has "obstacles", such as trees, which block the user from moving through the space occupied by the obstacle. 
+- [ ] The user can hear sound effects upon movement and upon catching a monster. 
+- [ ] The user hears background music during the game. 
+- [ ] The user can see their high score. 
+
+
+The following **additional** features are implemented:
+
+* [x] List anything else that you can get done to improve the page!
+
+## Time Spent and Lessons Learned
+
+Time spent: **X** hours spent in total.
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright [yyyy] [name of copyright owner]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ game.js
@@ -0,0 +1,154 @@
+/*
+  Code modified from:
+  http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/
+  using graphics purchased from vectorstock.com
+*/
+
+/* Initialization.
+Here, we create and add our "canvas" to the page.
+We also load all of our images. 
+*/
+
+
+let canvas;
+let ctx;
+
+canvas = document.createElement("canvas");
+ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+let bgReady, heroReady, monsterReady;
+let bgImage, heroImage, monsterImage;
+
+function loadImages() {
+  bgImage = new Image();
+  bgImage.onload = function () {
+    // show the background image
+    bgReady = true;
+  };
+  bgImage.src = "images/background.png";
+  heroImage = new Image();
+  heroImage.onload = function () {
+    // show the hero image
+    heroReady = true;
+  };
+  heroImage.src = "images/hero.png";
+
+  monsterImage = new Image();
+  monsterImage.onload = function () {
+    // show the monster image
+    monsterReady = true;
+  };
+  monsterImage.src = "images/monster.png";
+}
+
+/** 
+ * Setting up our characters.
+ * 
+ * Note that heroX represents the X position of our hero.
+ * heroY represents the Y position.
+ * We'll need these values to know where to "draw" the hero.
+ * 
+ * The same applies to the monster.
+ */
+
+let heroX = canvas.width / 2;
+let heroY = canvas.height / 2;
+
+let monsterX = 100;
+let monsterY = 100;
+
+/** 
+ * Keyboard Listeners
+ * You can safely ignore this part, for now. 
+ * 
+ * This is just to let JavaScript know when the user has pressed a key.
+*/
+let keysDown = {};
+function setupKeyboardListeners() {
+  // Check for keys pressed where key represents the keycode captured
+  // For now, do not worry too much about what's happening here. 
+  addEventListener("keydown", function (key) {
+    keysDown[key.keyCode] = true;
+  }, false);
+
+  addEventListener("keyup", function (key) {
+    delete keysDown[key.keyCode];
+  }, false);
+}
+
+
+/**
+ *  Update game objects - change player position based on key pressed
+ *  and check to see if the monster has been caught!
+ *  
+ *  If you change the value of 5, the player will move at a different rate.
+ */
+let update = function () {
+  if (38 in keysDown) { // Player is holding up key
+    heroY -= 5;
+  }
+  if (40 in keysDown) { // Player is holding down key
+    heroY += 5;
+  }
+  if (37 in keysDown) { // Player is holding left key
+    heroX -= 5;
+  }
+  if (39 in keysDown) { // Player is holding right key
+    heroX += 5;
+  }
+
+  // Check if player and monster collided. Our images
+  // are about 32 pixels big.
+  if (
+    heroX <= (monsterX + 32)
+    && monsterX <= (heroX + 32)
+    && heroY <= (monsterY + 32)
+    && monsterY <= (heroY + 32)
+  ) {
+    // Pick a new location for the monster.
+    // Note: Change this to place the monster at a new, random location.
+    monsterX = monsterX + 50;
+    monsterY = monsterY + 70;
+  }
+};
+
+/**
+ * This function, render, runs as often as possible.
+ */
+var render = function () {
+  if (bgReady) {
+    ctx.drawImage(bgImage, 0, 0);
+  }
+  if (heroReady) {
+    ctx.drawImage(heroImage, heroX, heroY);
+  }
+  if (monsterReady) {
+    ctx.drawImage(monsterImage, monsterX, monsterY);
+  }
+};
+
+/**
+ * The main game loop. Most every game will have two distinct parts:
+ * update (updates the state of the game, in this case our hero and monster)
+ * render (based on the state of our game, draw the right things)
+ */
+var main = function () {
+  update(); 
+  render();
+  // Request to do this again ASAP. This is a special method
+  // for web browsers. 
+  requestAnimationFrame(main);
+};
+
+// Cross-browser support for requestAnimationFrame.
+// Safely ignore this line. It's mostly here for people with old web browsers.
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+// Let's play this game!
+loadImages();
+setupKeyboardListeners();
+main();
